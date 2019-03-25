72568282a2c9f558346787832c6f4342e389c5f8
Gustavo Vicuña <gpvicuna@miuandes.cl>
2019-03-25 12:29:06 -0300

    Progress

--- app/src/main/java/com/example/lab01uandes/Login.kt
+++ app/src/main/java/com/example/lab01uandes/Login.kt
@@ -3,10 +3,7 @@ package com.example.lab01uandes
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-import android.provider.AlarmClock.EXTRA_MESSAGE
-
-
-
+import android.widget.Button
 
 
 class Login : AppCompatActivity() {
@@ -14,8 +11,10 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        val intent = intent
-
+    }
+    val button = findViewById<Button>(R.id.btn_login)
+    fun logIn(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent);
     }
 }
